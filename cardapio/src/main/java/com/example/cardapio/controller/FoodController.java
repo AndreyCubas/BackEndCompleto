be6e663b71e050b9013957810c97d03f03d20fa3
Andrey Felipe Cubas <Felipedreycubascardoso@gmail.com>
2024-11-04 21:47:09 -0300

    Segundo Commit

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,16 +1,25 @@
 package com.example.cardapio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@RestController//aqui vai falar com a web e responder ela
-@RequestMapping//aqui são os caminhos que vão ser usados para esse método
+import java.util.List;
 
+@RestController
+@RequestMapping("food")
 public class FoodController {
 
-    @GetMapping
-    public void getAll(){
+    @Autowired
+    private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
     }
 }
